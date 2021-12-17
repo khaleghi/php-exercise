090356f297be6d0d7dcb543a0374b1ae901933cb
Ahmadreza Khaleghi <xaleghi@gmail.com>
2021-12-17 13:12:15 +0330

    Add yii required php extensions
    - now yii is up!

--- app/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$pdo = new PDO('mysql:dbname=myappdb;host=mariadb', 'myapp', 'apppass', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-$query = $pdo->query('SHOW VARIABLES like "version"');
-
-$row = $query->fetch();
-
-echo 'MySQL version:' . $row['Value'];
-
--- app/public/info.php
+++
@@ -1,4 +0,0 @@
-<h1>PHP 8 Training...</h1>
-<?php
-phpinfo();
-?>
